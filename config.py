ff7c1e7ffb4c61dce96014ec8590c2832f89d55d
VENOM-CRAZY-OP <cyberecurityind@gmail.com>
2024-08-29 16:58:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7255048090:AAFpMpH9KDNrr3BpnlAlraE5c2UkqhlSY5w")
+BOT_TOKEN=("TOKEN")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("")
+OWNER_USERNAME=("venomXcrazy")
 
   #Enter your group id here Get it by creating a group that group add your bot and ross bot then public it and type /id to get your id
-GROUP ID=("")
+GROUP ID=("GROUPid")
 
 
 
